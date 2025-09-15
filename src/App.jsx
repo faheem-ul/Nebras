e60066fe2e35ce4d55e3cab14a187e3cd39f2ad1
Ayesha_Ramzan <168947876+AyeshaCodeCraft@users.noreply.github.com>
2025-09-15 03:24:04 -0700

    project set-up

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,13 @@
 
-import "./styles/global.css";
 
-
-export default function App() {
+function App() {
   return (
-    <div style={{ padding: "2rem" }}>
-      <h1 style={{ fontFamily: "AlYermook", }}>
-        This is AlYermook font
-      </h1>
-
-      <h1 style={{ fontFamily: "Arabic" }}>
-        السلام عليكم
-      </h1>
-
-      <h1 style={{ fontFamily: "KoSans" }}>
-        This is KoSans font
-      </h1>
-
-      <h1 style={{ fontFamily: "Logirent" }}>
-        This is Logirent font
-      </h1>
-
-      <h1 style={{ fontFamily: "Cortoba" }}>
-        This is Cortoba font
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-2xl font-bold text-gray-800">
+        Vite + React + Tailwind Setup
       </h1>
     </div>
   );
 }
 
-
-// import "./styles/global.css";
-
-// export default function App() {
-//   return (
-//     <div className="p-8 space-y-6">
-//       <h1 className="font-alyermook text-2xl text-red-500">This is AlYermook</h1>
-//       <h1 className="font-arabic text-2xl">السلام عليكم (Arabic Font)</h1>
-//     </div>
-//   );
-// }
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,26 @@
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue,svelte}", 
+  ],
   theme: {
     extend: {
       fontFamily: {
-        alyermook: ["AlYermook", "sans-serif"], 
-        arabic: ["Arabic", "sans-serif"],
+        alyermook: ["AlYermook", "sans-serif"],
+        arabic: ["Arabic", "serif"],
         kosans: ["KoSans", "sans-serif"],
         logirent: ["Logirent", "sans-serif"],
-        cortoba: ["Cortoba", "sans-serif"],
+        cortoba: ["Cortoba", "serif"],
       },
-    },
+      colors: {
+        darkGray: "#393C3D",
+        green: "#016938",
+        customBlack: "#070808",
+      }
+    }
   },
   plugins: [],
-}
+};
