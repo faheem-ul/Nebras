4e0386425079364a32853385a134973a6e98c16b
alihassamofficial <hassamali.official@gmail.com>
2025-09-18 11:25:58 +0500

    Added Routing to App, Centered Layout, Link to Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home/Home";
+import AboutUs from "./pages/AboutUS/AboutUs";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Home />
-
-      <Footer />
-    </>
+    <Router>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about-us" element={<AboutUs />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
-
-
 import { useState } from "react";
-import logo from "../assets/NavLogo.png"; 
+import { Link } from "react-router-dom";
+
+import logo from "../assets/NavLogo.png";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,20 +9,20 @@ const Navbar = () => {
   return (
     <nav className="w-full bg-white shadow-md pt-[97px] pb-[37px]">
       <div className="max-w-[1220px] mx-auto px-4 py-4 flex items-center justify-between">
-        
+
         {/* Left - Logo */}
         <img src={logo} alt="Logo" className="h-10 w-auto cursor-pointer" />
 
         {/* Right - Nav Links (Desktop) */}
         <div className="hidden md:flex items-center gap-8">
-            <a href="#" className="font-logirent font-normal text-lg leading-none hover:text-green-600">العربية</a>
-          <a href="#" className="font-logirent font-normal text-lg leading-none  hover:text-green-600">Home</a>
-          <a href="#" className="font-logirent font-normal text-lg leading-none  hover:text-green-600">Our Projects</a>
-          <a href="#" className="font-logirent font-normal text-lg leading-none hover:text-green-600">About Us</a>
-          <a href="#" className="font-logirent font-normal text-lg leading-none  hover:text-green-600">Insights</a>
-          
-          
-         <button className="ml-6 pl-[15px] pr-[15px] pt-[14px] pb-[14px] bg-green text-white cursor-pointer font-logirent font-normal text-lg leading-none">
+          <Link to="" className="font-logirent font-normal text-lg leading-none hover:text-green-600">العربية</Link>
+          <Link to="/" className="font-logirent font-normal text-lg leading-none  hover:text-green-600">Home</Link>
+          <Link to="" className="font-logirent font-normal text-lg leading-none  hover:text-green-600">Our Projects</Link>
+          <Link to="about-us" className="font-logirent font-normal text-lg leading-none hover:text-green-600">About Us</Link>
+          <Link to="" className="font-logirent font-normal text-lg leading-none  hover:text-green-600">Insights</Link>
+
+
+          <button className="ml-6 pl-[15px] pr-[15px] pt-[14px] pb-[14px] bg-green text-white cursor-pointer font-logirent font-normal text-lg leading-none">
             Contact Us
           </button>
         </div>
@@ -39,12 +39,12 @@ const Navbar = () => {
       {/* Mobile Menu */}
       {isOpen && (
         <div className="md:hidden bg-white px-4 pb-6 flex flex-col gap-4">
-            <a href="#" className="font-logirent font-normal text-lg leading-none text-gray-800 hover:text-green-600">العربية</a>
+          <a href="#" className="font-logirent font-normal text-lg leading-none text-gray-800 hover:text-green-600">العربية</a>
           <a href="#" className="font-logirent font-normal text-lg leading-none text-gray-800 hover:text-green-600">Home</a>
           <a href="#" className="font-logirent font-normal text-lg leading-none text-gray-800 hover:text-green-600">Our Projects</a>
           <a href="#" className="font-logirent font-normal text-lg leading-none text-gray-800 hover:text-green-600">About Us</a>
           <a href="#" className="font-logirent font-normal text-lg leading-none text-gray-800 hover:text-green-600">Insights</a>
-          
+
 
           <button className="mt-4 px-6 py-2 bg-green text-white rounded-md font-logirent font-normal text-lg leading-none">
             Contact Us
