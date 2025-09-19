2737328a6db7c9337ffdf7486cc45af95e13c71f
alihassamofficial <hassamali.official@gmail.com>
2025-09-19 18:36:55 +0500

    Added Nav Link to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,13 +11,13 @@ const Navbar = () => {
       <div className="max-w-[1312px] mx-auto px-5  flex items-center justify-between">
 
         {/* Left - Logo */}
-        <img src={logo} alt="Logo" className="h-auto w-[120px] md:w-[166px] cursor-pointer" />
+        <Link to="/"> <img src={logo} alt="Logo" className="h-auto w-[120px] md:w-[166px] cursor-pointer" /></Link>
 
         {/* Right - Nav Links (Desktop) */}
         <div className="hidden md:flex items-center justify-end gap-8 flex-wrap">
           <Link to="" className="font-logirent font-normal text-lg leading-none hover:text-green-600">العربية</Link>
           <Link to="/" className="font-logirent font-normal text-lg leading-none  hover:text-green-600">Home</Link>
-          <Link to="" className="font-logirent font-normal text-lg leading-none  hover:text-green-600">Our Projects</Link>
+          <Link to="our-projects" className="font-logirent font-normal text-lg leading-none  hover:text-green-600">Our Projects</Link>
           <Link to="about-us" className="font-logirent font-normal text-lg leading-none hover:text-green-600">About Us</Link>
           <Link to="insights" className="font-logirent font-normal text-lg leading-none  hover:text-green-600">Insights</Link>
 
@@ -37,21 +37,23 @@ const Navbar = () => {
       </div>
 
       {/* Mobile Menu */}
-      {isOpen && (
-        <div className="md:hidden bg-white px-4 pb-6 flex flex-col gap-4">
-          <a href="#" className="font-logirent font-normal text-lg leading-none text-gray-800 hover:text-green-600">العربية</a>
-          <a href="#" className="font-logirent font-normal text-lg leading-none text-gray-800 hover:text-green-600">Home</a>
-          <a href="#" className="font-logirent font-normal text-lg leading-none text-gray-800 hover:text-green-600">Our Projects</a>
-          <a href="#" className="font-logirent font-normal text-lg leading-none text-gray-800 hover:text-green-600">About Us</a>
-          <a href="#" className="font-logirent font-normal text-lg leading-none text-gray-800 hover:text-green-600">Insights</a>
+      {
+        isOpen && (
+          <div className="md:hidden bg-white px-4 pb-6 flex flex-col gap-4">
+            <a href="#" className="font-logirent font-normal text-lg leading-none text-gray-800 hover:text-green-600">العربية</a>
+            <a href="#" className="font-logirent font-normal text-lg leading-none text-gray-800 hover:text-green-600">Home</a>
+            <a href="#" className="font-logirent font-normal text-lg leading-none text-gray-800 hover:text-green-600">Our Projects</a>
+            <a href="#" className="font-logirent font-normal text-lg leading-none text-gray-800 hover:text-green-600">About Us</a>
+            <a href="#" className="font-logirent font-normal text-lg leading-none text-gray-800 hover:text-green-600">Insights</a>
 
 
-          <button className="mt-4 px-6 py-2 bg-green text-white rounded-md font-logirent font-normal text-lg leading-none">
-            Contact Us
-          </button>
-        </div>
-      )}
-    </nav>
+            <button className="mt-4 px-6 py-2 bg-green text-white rounded-md font-logirent font-normal text-lg leading-none">
+              Contact Us
+            </button>
+          </div>
+        )
+      }
+    </nav >
   );
 };
 
