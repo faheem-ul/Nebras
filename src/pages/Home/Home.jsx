726410f5543e23a8727abcf2d6a1457996b474f5
developer <developer@innovativemojo.com>
2025-09-19 12:11:31 +0500

    build issues resolved

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,14 @@
-
-
-
 // import Hero from "./sections/Hero";
 import WhatWeDo from "./sections/WhatWeDo";
 import OurProjects from "./sections/OurProjects";
 
+// import something from "public/vite.svg";
 
 export default function Home() {
   return (
     <main>
       {/* <Hero /> */}
+      {/* <img src={something} alt="Vite Logo" /> */}
       <WhatWeDo />
       <OurProjects />
     </main>
