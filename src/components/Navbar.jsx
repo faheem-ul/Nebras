f3b510c30184ed478be7635aa77794ff46f9de11
alihassamofficial <hassamali.official@gmail.com>
2025-09-22 13:20:06 +0500

    updated navbar menu link hover color, adjust menus spacing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,41 +43,41 @@ const Navbar = () => {
         />
 
         {/* Right - Nav Links (Desktop) */}
-        <div className="hidden md:flex items-center justify-end gap-8 flex-wrap">
+        <div className="hidden md:flex items-center justify-end gap-[43px] flex-wrap">
           <Link
             to=""
-            className="font-logirent font-normal text-lg leading-none hover:text-green-600"
+            className="font-logirent font-normal text-lg leading-none hover:text-[#016938]"
           >
             العربية
           </Link>
           <Link
             to="/"
-            className="font-logirent font-normal text-lg leading-none hover:text-green-600"
+            className="font-logirent font-normal text-lg leading-none hover:text-[#016938]"
           >
             Home
           </Link>
           <Link
             to="/our-projects"
-            className="font-logirent font-normal text-lg leading-none hover:text-green-600"
+            className="font-logirent font-normal text-lg leading-none hover:text-[#016938]"
           >
             Our Projects
           </Link>
           <Link
             to="about-us"
-            className="font-logirent font-normal text-lg leading-none hover:text-green-600"
+            className="font-logirent font-normal text-lg leading-none hover:text-[#016938]"
           >
             About Us
           </Link>
           <Link
             to="insights"
-            className="font-logirent font-normal text-lg leading-none hover:text-green-600"
+            className="font-logirent font-normal text-lg leading-none hover:text-[#016938]"
           >
             Insights
           </Link>
 
           <Link
             to="contact-us"
-            className="ml-6 pl-[15px] pr-[15px] pt-[14px] pb-[14px] bg-green text-white cursor-pointer font-logirent font-normal text-lg leading-none"
+            className="ml-3 pl-[15px] pr-[15px] pt-[14px] pb-[14px] bg-green text-white cursor-pointer font-logirent font-normal text-lg leading-none"
           >
             Contact Us
           </Link>
@@ -85,12 +85,11 @@ const Navbar = () => {
 
         {/* Mobile Hamburger */}
         <button
-          className={`md:hidden text-3xl z-50 ${
-            location.pathname === "/about-us" ||
+          className={`md:hidden text-3xl z-50 ${location.pathname === "/about-us" ||
             location.pathname === "/contact-us"
-              ? "text-white"
-              : "text-black"
-          }`}
+            ? "text-white"
+            : "text-black"
+            }`}
           onClick={() => setIsOpen(!isOpen)}
         >
           {isOpen ? "✕" : "☰"}
@@ -99,9 +98,8 @@ const Navbar = () => {
 
       {/* Fullscreen Mobile Menu */}
       <div
-        className={`fixed top-0 left-0 w-full h-full bg-white flex flex-col items-center justify-center gap-8 transition-transform duration-500 ease-in-out ${
-          isOpen ? "translate-x-0" : "-translate-x-full"
-        }`}
+        className={`fixed top-0 left-0 w-full h-full bg-white flex flex-col items-center justify-center gap-8 transition-transform duration-500 ease-in-out ${isOpen ? "translate-x-0" : "-translate-x-full"
+          }`}
       >
         {/* Force green logo inside menu */}
         <img src={greenLogo} alt="Logo" className="h-auto w-[140px] mb-8" />
@@ -109,35 +107,35 @@ const Navbar = () => {
         <Link
           onClick={handleClose}
           to=""
-          className="font-logirent text-2xl leading-none hover:text-green-600"
+          className="font-logirent text-2xl leading-none hover:text-[#016938]"
         >
           العربية
         </Link>
         <Link
           onClick={handleClose}
           to="/"
-          className="font-logirent text-2xl leading-none hover:text-green-600"
+          className="font-logirent text-2xl leading-none hover:text-[#016938]"
         >
           Home
         </Link>
         <Link
           onClick={handleClose}
           to="/our-projects"
-          className="font-logirent text-2xl leading-none hover:text-green-600"
+          className="font-logirent text-2xl leading-none hover:text-[#016938]"
         >
           Our Projects
         </Link>
         <Link
           onClick={handleClose}
           to="about-us"
-          className="font-logirent text-2xl leading-none hover:text-green-600"
+          className="font-logirent text-2xl leading-none hover:text-[#016938]"
         >
           About Us
         </Link>
         <Link
           onClick={handleClose}
           to="insights"
-          className="font-logirent text-2xl leading-none hover:text-green-600"
+          className="font-logirent text-2xl leading-none hover:text-[#016938]"
         >
           Insights
         </Link>
